b6a5421680732ff5ebd980fd8aaf51466152f527
anoop <anoop@earthianslive.com>
2018-08-29 20:58:22 +0530

    fixed js dt name

--- estimation/estimation/doctype/ess_project_estimate_item/ess_project_estimate_item.js
+++ estimation/estimation/doctype/ess_project_estimate_item/ess_project_estimate_item.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, earthians and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('eSs Project Estimate Item', {
+frappe.ui.form.on('eSS Project Estimate Item', {
 	refresh: function(frm) {
 
 	}
