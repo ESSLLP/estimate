53c6e9b6ada1314b45ec1c02a8ec11b4e1e1c4e7
anoop <anoop@earthianslive.com>
2018-08-29 19:31:34 +0530

    minor fixes to simulate mapdoc

--- estimation/estimation/doctype/ess_project_estimate/ess_project_estimate.py
+++ estimation/estimation/doctype/ess_project_estimate/ess_project_estimate.py
@@ -7,4 +7,11 @@ import frappe
 from frappe.model.document import Document
 
 class eSSProjectEstimate(Document):
-	pass
+	def on_submit(self):
+		if frappe.db.exists("Opportunity", self.opportunity):
+			opportunity_doc = frappe.get_doc("Opportunity", self.opportunity)
+			opportunity_doc.total_estimated_cost = self.total_estimated_cost
+			opportunity_doc.estimated_cost_of_boq_items = self.total_item_cost
+			opportunity_doc.estimated_expenses = self.estimated_expenses
+			opportunity_doc.estimated_activity_costs = self.estimated_activity_costs
+			opportunity_doc.save(ignore_permissions=True)
--- estimation/estimation/utils.py
+++ estimation/estimation/utils.py
@@ -23,13 +23,17 @@ def make_project_estimate(opportunity):
 	new_estimate.opportunity = opportunity_doc.name
 	new_estimate.estimation_date = nowdate()
 	new_estimate.customer_or_lead_name = opportunity_doc.customer_name
-
+	new_estimate.total_item_cost = 0
 	for opportunity_item in opportunity_doc.items:
 		new_item = new_estimate.append('items')
 		new_item.item_code = opportunity_item.item_code
 		new_item.qty = opportunity_item.qty
 		new_item.item_name = opportunity_item.item_name
 		new_item.uom = opportunity_item.uom
+		new_item.price_list_rate = frappe.get_value('Item Price', {'item_code': opportunity_item.item_code, 'price_list': "Standard Buying"}, 'price_list_rate')
+		new_item.amount = new_item.price_list_rate * new_item.qty
+		new_estimate.total_item_cost += new_item.amount
+	new_estimate.total_estimated_cost = new_estimate.total_item_cost
 	return new_estimate.as_dict()
 
 @frappe.whitelist()
@@ -45,12 +49,18 @@ def make_quotation(opportunity):
 	new_quote = frappe.new_doc("Quotation")
 	new_quote.opportunity = opportunity_doc.name
 	new_quote.estimation_date = nowdate()
+	new_quote.quotation_to = "Lead"
+	new_quote.lead = opportunity_doc.lead
 	new_quote.customer_or_lead_name = opportunity_doc.customer_name
-
+	new_quote.estimated_cost_of_boq_items = opportunity_doc.estimated_cost_of_boq_items
+	new_quote.estimated_expenses = opportunity_doc.estimated_expenses
+	new_quote.estimated_activity_costs = opportunity_doc.estimated_activity_costs
+	new_quote.total_estimated_cost = opportunity_doc.total_estimated_cost
 	for opportunity_item in opportunity_doc.items:
 		new_item = new_quote.append('items')
 		new_item.item_code = opportunity_item.item_code
 		new_item.qty = opportunity_item.qty
 		new_item.item_name = opportunity_item.item_name
 		new_item.uom = opportunity_item.uom
+		new_item.description = opportunity_item.item_name
 	return new_quote.as_dict()
